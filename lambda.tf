b1a0c2b19b57585c004f900ee52cd0c206c86cd8
DevOps01김은아 <96624366+manok119@users.noreply.github.com>
2022-05-28 11:42:48 +0900

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -75,7 +75,7 @@ resource "aws_lambda_permission" "sale_api" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:ap-northeast-2:694280818671:${aws_api_gateway_rest_api.sale_api.id}/*/${aws_api_gateway_method.sale_api.http_method}${aws_api_gateway_resource.sale_api.path}"
+  source_arn = "arn:aws:execute-api:ap-northeast-2:<유저 번호>:${aws_api_gateway_rest_api.sale_api.id}/*/${aws_api_gateway_method.sale_api.http_method}${aws_api_gateway_resource.sale_api.path}"
 }
 resource "aws_api_gateway_deployment" "sale_api" {
   rest_api_id = aws_api_gateway_rest_api.sale_api.id
